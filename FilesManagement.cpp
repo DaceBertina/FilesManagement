6b4256a814aacf428c13eb02146d68983025dccb
Dace Ķelpe-Bērtiņa <95188702+DaceBertina@users.noreply.github.com>
2023-12-03 23:34:55 +0200

    Search function.

--- FilesManagement.cpp
+++ FilesManagement.cpp
@@ -1,89 +1,124 @@
+#include <windows.h>
 #include <iostream>
-#include <filesystem>
-#include <queue>
-#include <chrono>
-#include <iomanip>
+#include <vector>
+#include <wchar.h>
 
 using namespace std;
-namespace fs = std::filesystem;
 
 struct FileInfo {
     wstring filename;
     wstring extension;
     uintmax_t size = 0;
-    fs::file_time_type creationTime = {};
-    fs::file_time_type modificationTime = {};
+    FILETIME creationTime = {};
+    FILETIME modificationTime = {};
+    DWORD attributes = 0;
 };
 
-void ProcessFilesInDirectoryQueue(const fs::path& directory, vector<FileInfo>& files) {
-    // Measure execution time
-    auto start = chrono::high_resolution_clock::now();
+vector<FileInfo> GetFilesInDirectory(const wstring& directory) {
+    vector<FileInfo> files;
 
-    queue<fs::path> directoryQueue;
-    directoryQueue.push(directory);
+    wstring searchPath = directory + L"\\*.*";
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind = FindFirstFile(searchPath.c_str(), &findFileData);
 
-    while (!directoryQueue.empty()) {
-        fs::path currentDir = directoryQueue.front();
-        directoryQueue.pop();
-
-        for (const auto& entry : fs::directory_iterator(currentDir)) {
-            if (entry.is_directory()) {
-                // Recursively get files from subdirectory
-                wstring subdirectory = entry.path().wstring();
-                directoryQueue.push(subdirectory);
-            }
-            else if (entry.is_regular_file()) {
-                // Process the file
-                FileInfo fileInfo;
-                fileInfo.filename = entry.path().filename().wstring();
-                fileInfo.size = fs::file_size(entry);
-                fileInfo.creationTime = fs::last_write_time(entry);
-                fileInfo.modificationTime = fs::last_write_time(entry);
-
-                // Extract extension from filename
-                size_t pos = fileInfo.filename.rfind(L'.');
-                if (pos != wstring::npos && pos < fileInfo.filename.size()) {
-                    fileInfo.extension = fileInfo.filename.substr(pos + 1);
-                }
-
-                files.push_back(fileInfo);
-            }
-        }
+    if (hFind == INVALID_HANDLE_VALUE) {
+        wcerr << L"Error opening directory: " << directory << endl;
+        return files;
     }
 
-    // Stop measuring time
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    do {
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            if (wcscmp(findFileData.cFileName, L".") != 0 && wcscmp(findFileData.cFileName, L"..") != 0) {
+                wstring subdirectory = directory + L"\\" + findFileData.cFileName;
+                vector<FileInfo> subdirectoryFiles = GetFilesInDirectory(subdirectory);
+                files.insert(files.end(), subdirectoryFiles.begin(), subdirectoryFiles.end());
+            }
+        }
+        else {
+            FileInfo fileInfo;
+            fileInfo.filename = findFileData.cFileName;
+            fileInfo.size = (static_cast<uintmax_t>(findFileData.nFileSizeHigh) << 32) | findFileData.nFileSizeLow;
+            fileInfo.creationTime = findFileData.ftCreationTime;
+            fileInfo.modificationTime = findFileData.ftLastWriteTime;
+            fileInfo.attributes = findFileData.dwFileAttributes;
 
-    // Display the execution time
-    wcout << L"Execution Time: " << duration.count() << L" milliseconds" << endl;
+            size_t pos = fileInfo.filename.rfind(L'.');
+            if (pos != wstring::npos) {
+                fileInfo.extension = fileInfo.filename.substr(pos + 1);
+            }
+
+            files.push_back(fileInfo);
+        }
+    } while (FindNextFile(hFind, &findFileData) != 0);
+
+    FindClose(hFind);
+    return files;
+}
+
+vector<wstring> SearchFilesByPartialName(const wstring& directory, const wstring& partialName) {
+    vector<wstring> matchingFiles;
+
+    wstring searchPath = directory + L"\\*.*";
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind = FindFirstFile(searchPath.c_str(), &findFileData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        wcerr << L"Error opening directory: " << directory << endl;
+        return matchingFiles;
+    }
+
+    do {
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            if (wcscmp(findFileData.cFileName, L".") != 0 && wcscmp(findFileData.cFileName, L"..") != 0) {
+                wstring subdirectory = directory + L"\\" + findFileData.cFileName;
+                vector<wstring> subdirectoryFiles = SearchFilesByPartialName(subdirectory, partialName);
+                matchingFiles.insert(matchingFiles.end(), subdirectoryFiles.begin(), subdirectoryFiles.end());
+            }
+        }
+        else {
+            wstring fileName = findFileData.cFileName;
+            if (_wcsistr(fileName.c_str(), partialName.c_str()) != nullptr) {
+                matchingFiles.push_back(directory + L"\\" + fileName);
+            }
+        }
+    } while (FindNextFile(hFind, &findFileData) != 0);
+
+    FindClose(hFind);
+    return matchingFiles;
 }
 
 int main() {
     wstring directory = L"C:\\Users\\User\\Desktop\\VIA_5_sem";
-    vector<FileInfo> files;
+    wstring partialName = L"cpp";
 
-    // Process files and collect information
-    ProcessFilesInDirectoryQueue(directory, files);
+    // Display matching files using Windows API
+    vector<wstring> matchingFiles = SearchFilesByPartialName(directory, partialName);
+    wcout << L"Matching Files using Windows API:\n";
+    for (const auto& file : matchingFiles) {
+        wcout << file << endl;
+    }
 
-    // Display the retrieved file information
-    wcout << L"\nQueue Method:\n";
+    // Display retrieved file information using recursive method
+    vector<FileInfo> files = GetFilesInDirectory(directory);
+    wcout << L"\nRecursive Method:\n";
     for (const auto& file : files) {
         wcout << L"File: " << file.filename << endl;
+        wcout << L"Attributes: " << file.attributes << endl;
         wcout << L"Extension: " << file.extension << endl;
         wcout << L"Size: " << file.size << L" bytes" << endl;
-        // Convert file_time_type to local time and print
-        auto creationTimePoint = chrono::time_point_cast<chrono::system_clock::duration>(file.creationTime - fs::file_time_type::clock::now() + chrono::system_clock::now());
-        auto modificationTimePoint = chrono::time_point_cast<chrono::system_clock::duration>(file.modificationTime - fs::file_time_type::clock::now() + chrono::system_clock::now());
-        time_t creationTime = chrono::system_clock::to_time_t(creationTimePoint);
-        time_t modificationTime = chrono::system_clock::to_time_t(modificationTimePoint);
-        tm creationSystemTime = {};
-        tm modificationSystemTime = {};
-        localtime_s(&creationSystemTime, &creationTime);
-        localtime_s(&modificationSystemTime, &modificationTime);
 
-        wcout << L"Creation Time: " << put_time(&creationSystemTime, L"%Y/%m/%d %H:%M:%S") << endl;
-        wcout << L"Modification Time: " << put_time(&modificationSystemTime, L"%Y/%m/%d %H:%M:%S") << endl;
+        SYSTEMTIME creationSystemTime, modificationSystemTime;
+        FileTimeToSystemTime(&file.creationTime, &creationSystemTime);
+        FileTimeToSystemTime(&file.modificationTime, &modificationSystemTime);
+
+        wcout << L"Creation Time: "
+            << creationSystemTime.wYear << L"/" << creationSystemTime.wMonth << L"/" << creationSystemTime.wDay
+            << L" " << creationSystemTime.wHour << L":" << creationSystemTime.wMinute << L":" << creationSystemTime.wSecond << endl;
+
+        wcout << L"Modification Time: "
+            << modificationSystemTime.wYear << L"/" << modificationSystemTime.wMonth << L"/" << modificationSystemTime.wDay
+            << L" " << modificationSystemTime.wHour << L":" << modificationSystemTime.wMinute << L":" << modificationSystemTime.wSecond << endl;
+
         wcout << L"---------------------------" << endl;
     }
 
